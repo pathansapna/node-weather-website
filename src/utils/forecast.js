217f80fb77562cd1a209eedd1cf9e0a4b7f5547d
pathansapna <https://github.com/pathansapna/java-app.git>
2021-04-02 17:41:09 +0530

    init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('postman-request')
+forecast = (latitude, longitude, callback)=>{
+const url = 'http://api.weatherstack.com/current?access_key=314ac3ccc1c67b528c70c51da85a23e1&query='+ latitude +',' + longitude + '&unit=f'
+    request({url, json: true}, (error, {body}) =>{
+        if (error){
+            callback('Unable to connect to weather service!',undefined)
+
+        }else if(body.error){
+            callback('Unable to find location . Try another search', undefined)
+
+        }else{
+
+            callback(undefined, 'It is currently ' + body.current.temperature + ' degrees out. It feels like ' +body.current.feelslike  + ' degrees out.');
+
+        }
+    })
+
+}
+module.exports = forecast
